707bdbdd24e39a7f5010cefd19490fe6c91caa4d
xSlendiX <gamingxslendix@gmail.com>
2020-06-12 22:17:17 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -108,7 +108,7 @@ echo "useradd -m ${username}; stty -echo; echo -e '${password}\n${password}' | p
 echo "usermod -aG wheel ${username}" | artools-chroot /mnt
 
 echo "chmod +w /etc/sudoers" | artools-chroot /mnt
-echo "echo 'Defaults	insults' > /etc/sudoers" | artools-chroot /mnt
+echo "echo 'Defaults insults' > /etc/sudoers" | artools-chroot /mnt
 echo "echo 'root ALL=(ALL) ALL' >> /etc/sudoers" | artools-chroot /mnt
 echo "echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers" | artools-chroot /mnt
 
