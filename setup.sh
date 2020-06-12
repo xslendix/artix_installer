0a80ba47b67a53367ed900eeb86fcde601dca439
xSlendiX <gamingxslendix@gmail.com>
2020-06-12 17:59:06 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,7 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
+if ping -q -w 1 -c 1 google.com > /dev/null then
     echo "Online! Continuing setup..."
 else
     echo "Offline! Quitting..."
