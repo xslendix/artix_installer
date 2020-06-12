e6453221ca73af14b98a40ce96fb0d020ac571e8
xSlendiX <gamingxslendix@gmail.com>
2020-06-12 17:54:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ if [ $laptop = "y" ] then
 	laptop=true
 else
 	laptop=false
-end
+fi
 
 echo "Laptop is now equal to ${laptop}."
 
@@ -39,4 +39,35 @@ echo -e "Dont forget to 'mkfs.ext4 -L BOOT /dev/sda4'\n\n^D to exit."
 
 bash
 
+echo "Mounting partitions..."
+
+echo -n "Mounting ROOT... "
+if [ -d /dev/disk/by-label/ROOT ]
+	mount /dev/disk/by-label/ROOT /mnt
+	echo "[  DONE  ]"
+else
+	echo "[ FAILED ]"	
+	echo "Cannot find ROOT partition! Quitting..."
+	exit 1
+fi
+
+mkdir /mnt/boot
+mkdir /mnt/home
+
+echo -n "Mounting HOME... "
+if [ -d /dev/disk/by-label/HOME ] then
+	mount /dev/disk/by-label/HOME /mnt/home
+	echo "[  DONE  ]"
+else
+	echo "[ IGNORE ]"
+fi
+
+echo -n "Mounting BOOT... "
+if [ -d /dev/disk/by-label/BOOT ] then
+	mount /dev/disk/by-label/BOOT /mnt/boot
+	echo "[  DONE  ]"
+else
+	echo "[ FAILED ]"
+	echo "Cannot find BOOT partition! Quitting..."
+fi
 
