679c0d5ac4efa8e628f2e2e25ab7e3e67d744f63
xSlendiX <gamingxslendix@gmail.com>
2020-06-13 13:42:46 +0300

    Update post.sh

--- post.sh
+++ post.sh
@@ -12,14 +12,31 @@ else
     exit 1
 fi
 
+echo -n "Username: "
+read username
+
 echo -n "Are you installing on a laptop? [y/N]: "
 read -n1 laptop
 
+noptimus=false
+
 if [ "$laptop" = "y" ]; then
 	laptop=true
+	
+	echo -n "Do you want to use nVidia Optimus? [y/N]: "
+	read -n1 noptimus
+
+	if [ "$noptimus" = "y" ]; then
+		noptimus=true
+	else
+		noptimus=false
+	fi
 else
 	laptop=false
 fi
+
+echo "Installing on laptop: ${laptop}"
+echo "Installing with nVidia Optimus: ${noptimus}"
 
 echo "Updating..."
 pacman -Syyu --noconfirm
@@ -30,11 +47,90 @@ pacman -S xorg --ignore xorg-server-xdmx --noconfirm
 echo "Installing nvidia drivers..."
 pacman -S nvidia-lts --noconfirm
 
-echo "Installing vim"
+echo "Installing vim..."
 pacman -S vim --noconfirm
 
-echo "Installing i3-gaps, polybar, dmenu, xcompmgr and feh"
+echo "Installing i3-gaps, polybar, dmenu, xcompmgr and feh..."
 pacman -S i3-gaps i3lock i3status polybar dmenu xcompmgr feh xfce4-clipman-plugin xfce4-screenshooter --noconfirm
+
+echo "Installing nvidia drivers..."
+pacman -S nvidia --noconfirm
+
+echo "Backing up xconfig..."
+if cp -v /etc/X11/xorg.conf /etc/X11/xorg.conf.old; then
+	echo "Configuring nVidia drivers..."
+	nvidia-xconfig
+else
+	echo "/etc/X11/xorg.conf could not be copied! It either doesn't exist or the disk has a problem. Abandoning nVidia config!"
+fi
+
+echo "Installing xrandr"
+pacman -S xorg-xrandr
+
+if $noptimus; then
+	echo "Configuring nVidia Optimus"
+	echo "Section "OutputClass"
+    Identifier "intel"
+    MatchDriver "i915"
+    Driver "modesetting"
+EndSection
+
+Section "OutputClass"
+    Identifier "nvidia"
+    MatchDriver "nvidia-drm"
+    Driver "nvidia"
+    Option "AllowEmptyInitialConfiguration"
+    Option "PrimaryGPU" "yes"
+    ModulePath "/usr/lib/nvidia/xorg"
+    ModulePath "/usr/lib/xorg/modules"
+EndSection" > /etc/X11/xorg.conf.d/10-nvidia-drm-outputclass.conf
+
+	sed -i '1s/^/xrandr --dpi 96\n/' /home/$username/.xinitrc
+	sed -i '1s/^/xrandr --auto\n/' /home/$username/.xinitrc
+	sed -i '1s/^/xrandr --setprovideroutputsource modesetting NVIDIA-0\n/' /home/$username/.xinitrc
+	
+	echo "#!/bin/sh
+xrandr --setprovideroutputsource modesetting NVIDIA-0
+xrandr --auto" > /etc/lightdm/display_setup.sh
+	chmod +x /etc/lightdm/display_setup.sh
+	
+	echo "Configuing LightDM"
+	echo "[LightDM]
+logind-check-graphical=true
+run-directory=/run/lightdm
+[Seat:*]
+greeter-session=lightdm-gtk-greeter     
+session-wrapper=/etc/lightdm/Xsession   
+display-setup-script=/etc/lightdm/display_setup.sh
+[XDMCPServer]
+[VNCServer]
+" > /etc/lightdm/lightdm.conf
+else
+	echo "Configuring LightDM"
+	echo "[LightDM]
+logind-check-graphical=true
+run-directory=/run/lightdm
+[Seat:*]
+greeter-session=lightdm-gtk-greeter     
+session-wrapper=/etc/lightdm/Xsession   
+[XDMCPServer]
+[VNCServer]
+" > /etc/lightdm/lightdm.conf
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
