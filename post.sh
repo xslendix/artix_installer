6eb930dadd55ede3595bff7d770bc261abd8a4a1
xSlendiX <gamingxslendix@gmail.com>
2020-06-13 16:07:17 +0300

    Update post.sh

--- post.sh
+++ post.sh
@@ -93,7 +93,7 @@ echo "Installing xrandr..."
 pacman -S xorg-xrandr --noconfirm
 
 echo "Installing LightDM..."
-pacman -S lightdm lightm-gtk-greeter displaymanager-openrc
+pacman -S lightdm lightm-gtk-greeter displaymanager-openrc xdm
 
 echo "Adding LightDM"
 rc-update add dbus default
